fa4cf9386dbe0afe1ddf5f27baaaaacd08b28d4c
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-01-09 00:58:56 +0700

    add toast, grid

---
+++ BlazorServer/Models/Employee1.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServer.Models
+{
+    public class Employee1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public DateOnly DOJ { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+}
