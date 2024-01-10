e4dc574b14144488b9711741c30700d0c4f0378c
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-01-10 11:12:21 +0700

    add  preload

---
+++ BlazorServer/Data/Migrations/20240110023328_personDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class personDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsNew",
+                table: "Person",
+                type: "bit",
+                nullable: true,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsNew",
+                table: "Person",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
